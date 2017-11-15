1f5dcee01a9992b451ae304bce4dbb3bf8b4dd1e
Ryan King <ryan@helloworldeng.com>
2017-11-15 08:53:47 -0800

    NPCs can get clicked on to 'start a conversation'

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+	float talkingDistance = 0.33f;
+	PlayerMovementController player;
+
+	// Use this for initialization
+	void Start () {
+		player = Object.FindObjectOfType<PlayerMovementController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var playerPos2D = Vector3.ProjectOnPlane (player.transform.position, Vector3.forward);
+		var pos2D = Vector3.ProjectOnPlane (transform.position, Vector3.forward);
+		if (Input.GetMouseButtonDown (0) && Vector3.Distance (playerPos2D, pos2D) <= talkingDistance) {
+			StartConversation ();
+		}
+	}
+
+	void StartConversation() {
+		Debug.Log ("Blah blah blah");
+	}
+}
