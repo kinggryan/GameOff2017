e1c3f091dc300fa4ddf60fcf72082b87d050f4ce
Ryan King <ryan@helloworldeng.com>
2017-11-30 20:26:58 -0800

    Added fade out title

---
+++ Assets/Scripts/FaderOuter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UnityEngine.UI.Text))]
+public class FaderOuter : MonoBehaviour {
+
+	float fadeoutTimer;
+	float fadeoutDelay = 2f;
+	float fadeoutDuration = 2f;
+	UnityEngine.UI.Text image;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<UnityEngine.UI.Text> ();
+		fadeoutTimer = fadeoutDelay + fadeoutDuration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fadeoutTimer -= Time.deltaTime;
+		var color = image.color;
+		color.a = Mathf.Clamp(fadeoutTimer / fadeoutDuration,0,1);
+		image.color = color;
+	}
+}
